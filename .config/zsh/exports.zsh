99c3e484f10ac2860cdd9e5604acb5d88e54c13d
aalaaiddine <aalaaiddine.ali-mlinde@epitech.eu>
2023-12-19 15:39:07 +0400

    zsh again

---
+++ .config/zsh/exports.zsh
@@ -0,0 +1,26 @@
+export PATH=/home/alaidine/.local/bin:$PATH
+export PATH=/home/alaidine/.cargo/env:$PATH
+export PATH=/home/alaidine/.cargo/bin:$PATH
+export PATH=/home/alaidine/go/bin:$PATH
+export PATH=/home/alaidine/My_scripts:$PATH
+export PATH=/home/alaidine/.config/tmux/plugins/t-smart-tmux-session-manager/bin:$PATH
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+export PATH=$PATH:/usr/local/go/bin
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+
+# Turso
+export PATH="/home/alaidine/.turso:$PATH"
+
+# Fly.io
+export FLYCTL_INSTALL="/home/alaidine/.fly"
+export PATH="$FLYCTL_INSTALL/bin:$PATH"
